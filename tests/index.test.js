3231481b21c7c23141944474053bd86530df4889
DAMN21 <dmezae@uninorte.edu.co>
2021-11-10 00:32:52 -0500

    delete: 🔥 archivos eliminados tests + jest.config.js

--- tests/index.test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-const app = require('../app');
-
-describe('GET /index', () => {
-  it('Should response hello world-index', async () => {
-    const response = await request(app).get('/');
-    expect(response.body.data).toBe('Hello World-index');
-  });
-});
